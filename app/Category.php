a566b723829ca237237bc0e3b030844014e06bf6
Fran <fjloopez@gmail.com>
2017-08-07 00:05:14 -0300

    update

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+
+    protected $fillable = ['id', 'description', 'created_at', 'updated_at', 'category_img'];
+    protected $dates = ['created_at', 'updated_at'];
+
+
+    public function questions()
+    {
+        return $this->hasMany('App\Question');
+    }
+
+
 }
--- app/Http/Controllers/LogoutController.php
+++ app/Http/Controllers/LogoutController.php
@@ -10,8 +10,6 @@ class LogoutController extends Controller
 {
     public function getLogout()
     {
-//    	$this->auth->logout();
-//    	return view('front.main.index');
         Auth::logout();
         Session::flush();
         return redirect('/vikingtrivia');
