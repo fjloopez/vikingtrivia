4d014b9faf95b1bdbd7f21faa60fca96304044da
AlanCebohin <cebo_gc@hotmail.com>
2017-08-07 00:06:02 -0300

    Register desalineado

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/login.blade.php';
+    protected $redirectTo = '/home';
 
     /**
      * Create a new controller instance.
