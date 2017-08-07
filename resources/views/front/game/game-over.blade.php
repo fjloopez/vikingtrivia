2fd46938aee18895ebf206a9fd1ef75fcad025d8
AlanCebohin <cebo_gc@hotmail.com>
2017-08-07 18:46:26 -0300

    la nada misma

---
+++ resources/views/front/game/game-over.blade.php
@@ -0,0 +1,7 @@
+@extends('front.app')
+
+@section('title','/vikingtrivia/juego')
+
+@section('content')
+	<h1>GAME OVER</h1>
+@endsection
--- resources/views/front/game/index.blade.php
+++ resources/views/front/game/index.blade.php
@@ -8,9 +8,9 @@
 	<div class="question">$question</div>
 
 	<div class="question_container">
-		<div class="answers"><a href="front.game.index2"></a>$answer1</div>
-		<div class="answers"><a href="front.game.index2"></a>$answer2</div>
-		<div class="answers"><a href="front.game.index2"></a>$answer3</div>
-		<div class="answers"><a href="front.game.index2"></a>$answer4</div>
+		<div class="answers"><a href="front.game.result"></a>$answer1</div>
+		<div class="answers"><a href="front.game.result"></a>$answer2</div>
+		<div class="answers"><a href="front.game.result"></a>$answer3</div>
+		<div class="answers"><a href="front.game.result"></a>$answer4</div>
 	<div>
 @endsection
