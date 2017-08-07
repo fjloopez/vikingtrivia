6a1547ceba4f9f218e7156a3c3a71fe745f88359
Fran <fjloopez@gmail.com>
2017-08-06 23:52:17 -0300

    update logout

--- app/Http/Controllers/LogoutController.php
+++ app/Http/Controllers/LogoutController.php
@@ -2,14 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class LogoutController extends Controller
 {
     public function getLogout()
     {
-    	$this->auth->logout();
-    	return view('front.main.index');
+//    	$this->auth->logout();
+//    	return view('front.main.index');
+        Auth::logout();
+        Session::flush();
+        return redirect('/vikingtrivia');
     }
 }
