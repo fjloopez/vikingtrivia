0fe0efd15952c41f414064727c28f59c58e285ee
Fran <fjloopez@gmail.com>
2017-08-07 01:01:50 -0300

    update menu log user

--- resources/views/front/menu/menu-log-user.blade.php
+++ resources/views/front/menu/menu-log-user.blade.php
@@ -3,7 +3,7 @@
         <ul class="list_menu">
             <li><a href="/vikingtrivia"><h4>Inicio</h4></a></li>
             <li><a href="/vikingtrivia/profile"><h4>Perfil</h4></a></li> <!-- href='profile.php' -->
-            <li><a href="/vikingtrivia/faqs"><h4>FAQs</h4></a></li>
+            <li><a href="/vikingtrivia/play"><h4>Jugar</h4></a></li>
             <li><a href="/vikingtrivia/logout" onclick="alert('Seguro desea desloguearse?')"><h4>Cerrar Sesión</h4></a></li>
             <li>Ya somos <a id="userCount"></a> vikingos</li>
         </ul>
