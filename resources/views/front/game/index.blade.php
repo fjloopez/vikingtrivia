1f0cc8c10751b07e5b219a59331370412fbc09be
AlanCebohin <cebo_gc@hotmail.com>
2017-08-07 01:38:11 -0300

    Principio de vista del juego

--- resources/views/front/game/index.blade.php
+++ resources/views/front/game/index.blade.php
@@ -1,1 +1,16 @@
 @extends('front.app')
+
+@section('title','/vikingtrivia/juego')
+
+@section('content')
+	<div class="categoryImage">$categoryImage</div>
+
+	<div class="question">$question</div>
+
+	<div class="question_container">
+		<div class="answers"><a href="front.game.index2"></a>$answer1</div>
+		<div class="answers"><a href="front.game.index2"></a>$answer2</div>
+		<div class="answers"><a href="front.game.index2"></a>$answer3</div>
+		<div class="answers"><a href="front.game.index2"></a>$answer4</div>
+	<div>
+@endsection
