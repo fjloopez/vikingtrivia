25b004338beb82d75efbcaed92e7024afb0eca03
Ruypo <ruypo00@gmail.com>
2017-08-08 10:03:01 -0300

    Ultimo Push

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
-@extends('layouts.app')
+@extends('front.app')
 
 @section('content')
-<div class="container">
+<div class="container_register">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
