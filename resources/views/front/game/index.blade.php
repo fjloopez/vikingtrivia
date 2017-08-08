acfd604adf7f5336410696a48dea9dbd66133622
Fran <fjloopez@gmail.com>
2017-08-08 03:57:26 -0300

    game view

--- resources/views/front/game/index.blade.php
+++ resources/views/front/game/index.blade.php
@@ -1,16 +1,18 @@
 @extends('front.app')
 
-@section('title','/vikingtrivia/juego')
+@section('title', "Jugar")
+
 
 @section('content')
-	<div class="categoryImage">$categoryImage</div>
-
-	<div class="question">{{ $question }}</div>
-
-	<div class="question_container">
-		<div class="answers"><a href="front.game.result"></a>$answer1</div>
-		<div class="answers"><a href="front.game.result"></a>$answer2</div>
-		<div class="answers"><a href="front.game.result"></a>$answer3</div>
-		<div class="answers"><a href="front.game.result"></a>$answer4</div>
 	<div>
+		<div class="question"><h2>{{ $question->text }}</h2></div>
+
+		<div class="question_container">
+			<ul>
+				@foreach($answers as $answer)
+					<li><button><a href="front.game.result">{{ $answer->text }}</a></button></li>
+				@endforeach
+			</ul>
+		<div>
+	</div>
 @endsection
