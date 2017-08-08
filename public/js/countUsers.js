29bdc2d6c93bf681cdce90968880c9f4a14244a1
Ruypo <ruypo00@gmail.com>
2017-08-08 02:56:48 -0300

    fix en el countUsers.js

--- public/js/countUsers.js
+++ public/js/countUsers.js
@@ -15,6 +15,6 @@ function countUsers() {
             document.getElementById("userCount").innerHTML = this.responseText;
         }
     };
-    xmlhttp.open("GET","count-users",true);
+    xmlhttp.open("GET","/count-users",true);
     xmlhttp.send();
 }
