81290fc49f4408f0c7d0b52006870520e09ec3a5
Ruypo <ruypo00@gmail.com>
2017-08-08 04:21:54 -0300

    Game y CSS

--- resources/views/front/game/index.blade.php
+++ resources/views/front/game/index.blade.php
@@ -4,15 +4,15 @@
 
 
 @section('content')
-	<div>
+	<div class="container_register">
 		<div class="question"><h2>{{ $question->text }}</h2></div>
 
 		<div class="question_container">
 			<ul>
 				@foreach($answers as $answer)
-					<li><button><a href="front.game.result">{{ $answer->text }}</a></button></li>
+					<li class="play_li"><button><a class="play_a_laravel" href="front.game.result">{{ $answer->text }}</a></button></li>
 				@endforeach
 			</ul>
-		<div>
+		</div>
 	</div>
 @endsection
