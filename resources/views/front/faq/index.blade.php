dd5cd2ae1d01ed8a2f0f80fc2b91e853689d786c
Ruypo <ruypo00@gmail.com>
2017-08-07 21:39:26 -0300

    Nueva sección FAQs

--- resources/views/front/faq/index.blade.php
+++ resources/views/front/faq/index.blade.php
@@ -7,48 +7,43 @@
 	<div class="container_faq"> <!-- abre container faq -->
 
 	<a href="#container_answer_one">
-		<h3 class="question_one">¿Qué es Viking Adventures?</h3>
+		<h3 class="question_one">¿Qué es Viking Trivia?</h3>
 	</a>
 
 	<div class="container_answer_one" id="container_answer_one"> <!-- Abre Respuesta uno -->
 		<p class="answer_one">
-			Viking Adventures es un juego al estilo novela gráfica, donde uno puede elegir su propio
-			camino y crear su historia dependiendo de las desiciones que tome.
+			Viking Trivia es un juego de preguntas y respuestas, un clásico juego de trivia, donde vas a poder llegar a un puntaje en base a tu conocimiento sobre la vida y logros de los vikingos.
 		</p>
 	</div> <!-- Cierra Respuesta uno -->
 
 
 	<a href="#container_answer_two">
-		<h3 class="question_two">¿Cómo hago para elegir mi propio camino?</h3>
+		<h3 class="question_two">¿Cómo se juega?</h3>
 	</a>
 
 	<div class="container_answer_two" id="container_answer_two"> <!-- Abre Respuesta dos -->
 		<p class="answer_two">
-			A lo largo del viaje, al personaje principal se le van dando distintas opciones sobre las situaciones
-			en las cuales se va encontrando. Éstas se pueden elegir con los botones que aparecen al fondo de la
-			pantalla, al estilo "multiple choice".
+			Al apretar el botón de "Jugar" se inicia una serie de preguntas, cada una va a tener 4 opciones dentro de las cuales sólo una va a ser la correcta. Éstas respuestas se pueden elegir con los botones que aparecen al fondo de la pantalla, al estilo "multiple choice", y la idea es llegar a la mayor cantidad de aciertos posible.
 		</p>
 	</div> <!-- Cierra Respuesta dos -->
 
 	<a href="#container_answer_three">
-		<h3 class="question_three">¿Los personajes son los que se ven en el inicio?</h3>
+		<h3 class="question_three">¿Puedo jugar con amigos?</h3>
 	</a>
 
 	<div class="container_answer_three" id="container_answer_three"> <!-- Abre Respuesta tres -->
 		<p class="answer_three">
-			Si, uno puede elegir entre el personaje masculino y el femenino. Aunque los dos comiencen en el mismo
-			lugar, el hecho de ser varón o mujer abre distintas posibilidades a lo largo del viaje.
+			Sí, a medida que se van adquiriendo puntos con las respuestas acertadas, se van sumando a un puntaje de la cuenta (si la cuenta está logueada) para poder competir con tus amigos. Tratá de ver quién puede llegar al puntaje más alto.
 		</p>
 	</div> <!-- Cierra Respuesta tres -->
 
 	<a href="#container_answer_four">
-		<h3 class="question_four">¿Hay más de un final?</h3>
+		<h3 class="question_four">¿Hay más de un tema?</h3>
 	</a>
 
 	 <div class="container_answer_four" id="container_answer_four"> <!-- Abre Respuesta cuatro -->
 		<p class="answer_four">
-			Si, la idea es que las desiciones que uno toma puedan llevar a finales completamente distintos. Todavía no
-			establecimos una cantidad fija de finales, ya que vamos a ir implementando cada vez más.
+			Sí, el juego se divide en tres categorías: "Armas y Guerreros", "Viajes y Conquistas", y "Cultura y Costumbres".
 		</p>
 	</div> <!-- Cierra Respuesta cuatro -->
 
